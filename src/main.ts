0735e456e909939133bb1db892dd1b3973e1c0db
Roman Claudiu <roman.claudiumihai@gmail.com>
2025-11-14 19:45:39 +0200

    5-define-users-resource-starter

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { INestApplication } from '@nestjs/common';
+
+function configureSwagger(app: INestApplication) {
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Js Heroes API')
+    .setDescription('This is a nice api')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('docs', app, document);
+}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  configureSwagger(app);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
