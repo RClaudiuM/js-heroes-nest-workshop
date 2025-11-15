8ccd03d4695123717fc9ff95dc607467862d982c
Razvan <rchitea@gmail.com>
2025-11-15 12:58:18 +0200

    first commit

---
+++ src/pokemons/pokemons.controller.ts
@@ -0,0 +1,17 @@
+import { Controller, Get, Param, Query } from '@nestjs/common';
+import { PokemonsService } from './pokemons.service';
+
+@Controller('pokemons')
+export class PokemonsController {
+  constructor(private readonly pokemonsService: PokemonsService) {}
+
+  @Get()
+  findAll(@Query("type") type?: string) {
+    return this.pokemonsService.findAll(type);
+  }
+
+  @Get(':id')
+  findOne(@Param("id") id: string){
+    return this.pokemonsService.findOne(id);
+  }
+}
